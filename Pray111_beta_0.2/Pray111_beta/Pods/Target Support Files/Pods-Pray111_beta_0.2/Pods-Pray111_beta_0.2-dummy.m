f15de21db5d783cf57277e89bcf947105fb20558
hs <>
2021-03-10 14:49:55 +0900

    resume

--- Pray111_beta_0.2/Pray111_beta/Pods/Target Support Files/Pods-Pray111_beta_0.2/Pods-Pray111_beta_0.2-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Pray111_beta_0_2 : NSObject
-@end
-@implementation PodsDummy_Pods_Pray111_beta_0_2
-@end
--- Pray111_beta_0.2/Pray111_beta/Pray111_beta_0.2/WriteController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WriteController.swift
-//  TableView
-//
-//  Created by vlm on 2020/10/21.
-//  Copyright © 2020 vlm. All rights reserved.
-//
-
-import Foundation
